fa0c056c7476c68bd9fc3cdd17818b43679ae3eb
nightmarkets <test>
2016-08-23 15:59:31 -0700

    session1_allAssignments

---
+++ Ass1Sum.java
@@ -0,0 +1,28 @@
+package com.session1.ass1;
+
+public class Ass1Sum {
+public static void main (String[] arg)
+{
+	//Below is for Assignment 1
+	int intNumber = 1;
+	float flNumber =2.3f;
+	System.out.println("Sum = "+ (int)(intNumber+flNumber));
+	
+	//Below is for Assignment 2
+	int a, b=4, c;
+	System.out.println("b=" + b);
+	
+	//Below is for Assignment 3
+	//Give two examples of data that cannot be stored in a variable of type int
+	// int = 'c'; 
+	// int = "word string";
+	
+	//Below is for Assignment 4
+	int x=5;
+	int y=10;
+	System.out.println("x+y*2="+ (x+y*2));
+	System.out.println("x-y+2="+(x-y+2));
+	System.out.println("(x+y)*2="+ ((x+y)*2));
+	System.out.println("y%x="+ (y%x));
+}
+}
